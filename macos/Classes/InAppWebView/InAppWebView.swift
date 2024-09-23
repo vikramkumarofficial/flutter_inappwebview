7965da3f177873d870a6cf136b28a523572a4d7f
Vikram Kumar <vikramkumar.official@gmail.com>
2024-09-23 15:15:30 +0530

    xcode 16 fix

--- macos/Classes/InAppWebView/InAppWebView.swift
+++ macos/Classes/InAppWebView/InAppWebView.swift
@@ -850,8 +850,8 @@ public class InAppWebView: WKWebView, WKUIDelegate,
         }
     }
     
-    public override func evaluateJavaScript(_ javaScriptString: String, completionHandler: ((Any?, Error?) -> Void)? = nil) {
-        if let applePayAPIEnabled = settings?.applePayAPIEnabled, applePayAPIEnabled {
+ public override func evaluateJavaScript(_ javaScriptString: String, completionHandler: (@MainActor @Sendable (Any?, (any Error)?) -> Void)? = nil) {        
+    if let applePayAPIEnabled = settings?.applePayAPIEnabled, applePayAPIEnabled {
             if let completionHandler = completionHandler {
                 completionHandler(nil, nil)
             }
